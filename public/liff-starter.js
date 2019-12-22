c9e7d54f662964f42735f65d8fbeb0aec4f2821e
eugencong <43124968+eugencong@users.noreply.github.com>
2019-12-22 23:16:21 +0700

    test

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -105,7 +105,7 @@ function registerButtonHandlers() {
     // openWindow call
     document.getElementById('openWindowButton').addEventListener('click', function() {
         liff.openWindow({
-            url: 'https://line.me',
+            url: 'game/index.html',
             external: true
         });
     });
